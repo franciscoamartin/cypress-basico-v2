bce1b990ec24f0159caabade37bde39955d87ad1
Francisco Augusto Martin <francisco.martin@hbsis.com.br>
2022-05-04 14:57:13 -0300

    add upload file tests

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -113,4 +113,31 @@ describe("Central de Atendimento ao cliente TAT", function () {
       .uncheck()
       .should("not.be.checked");
   });
+
+  it("seleciona um arquivo da pasta fixture", function () {
+    cy.get('input[type="file"]')
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo simulando drag-and-drop", function () {
+    cy.get('input[type="file"]')
+      .should("not.have.value")
+      .selectFile("./cypress/fixtures/example.json", { action: "drag-drop" })
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
+
+  it("seleciona um arquivo utilizando uma fixture que foi dada um alias", function () {
+    cy.fixture("example.json").as("sampleFile");
+    cy.get('input[type="file"]')
+      .selectFile("@sampleFile")
+      .should(function ($input) {
+        expect($input[0].files[0].name).to.equal("example.json");
+      });
+  });
 });
